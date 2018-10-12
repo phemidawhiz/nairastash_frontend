6647922083d6d91f99468a6835503b8386399365
Ayodele Olufemi <o.ayodele@verifyme.ng>
2018-10-12 12:32:31 +0100

    production updates

---
+++ login/js/login.js
@@ -0,0 +1,226 @@
+$(document).ready(function() {
+
+  //process login form
+  $('#loginForm').on('submit', function(event) {
+    event.preventDefault();
+    $('#loginBtn').attr('disabled', true);
+    $('#loginBtn').attr('value', "Processing...");
+    var xhr = new XMLHttpRequest();
+    var email = $('#email').val();
+    var password = $('#password').val();
+    var formData = JSON.stringify({
+      	"email":email,
+      	"password":password
+      });
+      console.log(formData);
+      xhr.onreadystatechange = function() {//Call a function when the state changes.
+         if(xhr.readyState === 4) {
+           console.log(this.responseText);
+           var response = JSON.parse(this.responseText);
+           var responseMsg = response["message"];
+           var regcode = response["regcode"];
+           var useremail = response["email"];
+           var urole = response["urole"];
+           if(response["status"] === true) {
+             swal("Login Successful! Reloading...", {
+              buttons: false
+            });
+             $('#loginBtn').attr('Submit');
+             $('#loginBtn').attr('disabled', false);
+             $('#loginBtn').attr('value', "Submit");
+             setCookie('useremail', useremail, 3);
+             setCookie('regcode', regcode, 3);
+             setCookie('urole', urole, 3);
+             var getUserRole = getCookie("urole");
+             if(getUserRole === "4") {
+               window.location.assign(webdomain+"/pages/profile/admin.php");
+             } else {
+               window.location.assign(webdomain+"/pages/profile");
+             }
+             //location.reload(true);
+           } else {
+             if(responseMsg == "Your account is not activated yet") {
+               swal({
+                   title: "Unable to login",
+                   text: "Your account is not activated yet. Please make payment to activate your account",
+                   icon: "warning",
+                   buttons: ["Cancel", "Pay Now"]
+                 })
+                 .then(function(willPay) {
+                   if (willPay) {
+                     payWithPaystack(email);
+
+                   } else {
+                     $('#registerBtn').attr('Submit');
+                     $('#registerBtn').attr('disabled', false);
+                     $('#registerBtn').attr('value', "Submit");
+                     swal("Please Note", "You will not be able to access your account until you make payment.", "info");
+                   }
+                 });
+             } else {
+               swal("Unable to login", responseMsg, "warning");
+             }
+             $('#loginBtn').attr('disabled', false);
+             $('#loginBtn').attr('value', "Submit");
+           }
+         }
+      }
+      xhr.open("POST", apidomain+"/login.php");
+      xhr.setRequestHeader('Content-Type', 'application/json');
+      xhr.send(formData);
+  });
+
+  //process password recovery form
+  $('#prForm').on('submit', function(event) {
+    event.preventDefault();
+    $('#prBtn').attr('disabled', true);
+    $('#prBtn').attr('value', "Processing...");
+    var xhr = new XMLHttpRequest();
+    var newpassword = $('#newpassword').val();
+    var conpassword = $('#conpassword').val();
+    var token = getUrlParameter('prltoken');
+
+    if(newpassword !== conpassword) {
+      swal("Mismatched Passwords", "Passwords must be the same", "warning");
+      $('#prBtn').attr('value', "Update");
+      $('#prBtn').attr('disabled', false);
+    } else {
+        xhr.onreadystatechange = function() {//Call a function when the state changes.
+           if(xhr.readyState === 4) {
+
+             var response = JSON.parse(this.responseText);
+             var responseMsg = response["message"];
+
+             if(response["status"] === true) {
+
+                 swal({
+                     title: "Done!",
+                     text: responseMsg,
+                     icon: "success",
+                     buttons: ["Close", "Login"]
+                   })
+                   .then(function(login) {
+                     if (login) {
+                       window.location.assign(webdomain+"/pages/login");
+                     } else {
+                       $('#prBtn').attr('value', "Update");
+                       $('#prBtn').attr('disabled', false);
+                     }
+                   });
+
+               $('#prBtn').attr('value', "Update");
+             } else {
+               $('#prBtn').attr('disabled', false);
+               $('#prBtn').attr('value', "Update");
+               swal("Unable to reset Password", responseMsg, "error");
+             }
+           }
+        }
+        xhr.open("GET", apidomain+"/passwordreset/?actiontype=resetpwd&newpassword=" + newpassword + "&prltoken=" + token);
+        xhr.send();
+    }
+
+  });
+
+  //Pay With Paystack
+  function payWithPaystack(mememail){
+    swal("Please wait while payment page loads...", {
+     buttons: false,
+     timer: 3000
+    });
+
+    var handler = PaystackPop.setup({
+        key: 'pk_live_c681b5e7255499bb4e656973403e9d72bcc2dcee',
+        email: mememail,
+        amount: 200000,
+        ref: 'PE_REG_FEE' + Math.floor((Math.random() * 1000000000) + 1),
+        callback: function(response) {
+            /* swal("Please wait while we get response from paystack", {
+             buttons: false,
+             timer: 4000
+           }); */
+            var xhr = new XMLHttpRequest();
+            xhr.withCredentials = false;
+            xhr.addEventListener("readystatechange", function() {
+                if (this.readyState === 4) {
+                    var a = JSON.parse(this.responseText);
+                    if (a.status == "success") {
+                      var xhr = new XMLHttpRequest();
+                      xhr.withCredentials = false;
+                      xhr.addEventListener("readystatechange", function() {
+                          if (this.readyState === 4) {
+                              var a = JSON.parse(this.responseText);
+                              if (a.status == true) {
+                                  swal("Account Activated!", a.message, "success");
+                              } else {
+                                swal("Account Activation Failed", a.message, "error");
+                              }
+                          }
+                      });
+
+                      xhr.open("GET", apidomain+"/logpayment/?mememail=" + mememail + "&refcode=" + response.reference);
+                      xhr.send(data);
+
+                    } else {
+                      swal("Payment completed!", "Your account will be activated within 24 hours.", "success");
+                    }
+                }
+            });
+
+            xhr.open("GET", "https://api.paystack.co/transaction/verify/" + response.reference);
+            xhr.setRequestHeader("authorization",
+                "Bearer sk_live_ca522dfef89254637cb49578d01425d4433bca62");
+            xhr.send();
+        },
+        onClose: function() {
+
+        }
+    });
+    handler.openIframe();
+  }
+
+  $('#forgotPassword').click(function() {
+    swal({
+      text: 'Please provide the email you registered to get a password reset link',
+      content: "input",
+      button: {
+        text: "Submit",
+        closeModal: true,
+      },
+    })
+    .then(function(name) {
+      if (!name) throw null;
+
+      return fetch(`https://nairastash.com/api/passwordreset/?prlemail=${name}&actiontype=sendlink`);
+    })
+    .then(function(results) {
+      return results.json();
+    })
+    .then(function(json) {
+      const prlData = json.message;
+
+      if (json.status == false) {
+        return swal("Unable to send link", json.message, "info");
+      } else {
+        return swal("Password reset link sent", json.message, "success");
+      }
+
+
+    })
+    .catch(function(err) {
+      if (err) {
+        swal("Oh noes!", "We were unable to send a link to that email!", "error");
+      } else {
+        swal.stopLoading();
+        swal.close();
+      }
+    });
+  });
+
+  // check if user is logged in
+  var getUN = getCookie("useremail");
+  if (getUN !== "") {
+      window.location.assign(webdomain+"/pages/profile");
+  }
+
+});
---
+++ register/js/register.js
@@ -0,0 +1,188 @@
+$(document).ready(function() {
+
+  //initial settings
+  var ur = window.location.href;
+  console.log(ur);
+  var getRefCode = getCookie("refcode");
+	var msg = getRefCode + "from cookie";
+	console.log(msg);
+  var firstcode = "";
+  // check if there is a referral code in cookie
+  var msg2 = getUrlParameter('refcode') + " from url";
+  console.log(msg2);
+  if (getRefCode == "") {
+    if(ur.includes("refcode")) {
+      $('#rcLabel').css('display', 'none');
+      $('#firstcode').hide();
+      $('#firstcode').attr('required', false);
+      firstcode = getUrlParameter('refcode');
+      console.log(firstcode + " from url");
+      increaseActivityScore(3, firstcode);
+    } else {
+      $('#rcLabel').css('display', 'block');
+      $('#firstcode').show();
+      // store reg code of last registered member into cookie
+    }
+
+  } else {
+    $('#rcLabel').css('display', 'none');
+    $('#firstcode').hide();
+    $('#firstcode').attr('required', false);
+    firstcode = getRefCode;
+    console.log(firstcode + " from cookie");
+  }
+
+  //function to increase activity score
+  function increaseActivityScore(score, refcode) {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+          var jsonObj = JSON.parse(this.responseText);
+          if(jsonObj["status"] === true) {
+
+        }
+      }
+    }
+    xhr.open("GET", apidomain+"/increaseactivityscore/?score=" + score + "&refcode=" + refcode);
+    xhr.send();
+  }
+
+  //process registeration form
+  $('#registerForm').on('submit', function(event) {
+    event.preventDefault();
+    $('#registerBtn').attr('disabled', true);
+    $('#registerBtn').attr('value', "Processing...");
+    var xhr = new XMLHttpRequest();
+    var fullname = $('#fullname').val();
+    var phonenumber = $('#phonenumber').val();
+    var acctnum = $('#acctnum').val();
+    var password = $('#password').val();
+    var mememail = $('#mememail').val();
+    var address = $('#address').val();
+    var rcode = "";
+    if (firstcode == "") {
+      rcode = $('#firstcode').val();
+    } else {
+      rcode = firstcode;
+    }
+    
+    var occupation = $('#occupation').val();
+    var bankname = $('#bankname').val();
+    var conpassword = $('#conpassword').val();
+    if(password !== conpassword) {
+      swal("Password Mismatch", "Please Check Passwords", "warning");
+      $('#registerBtn').html('<i class="fa fa-plus"></i> Submit');
+      $('#registerBtn').attr('disabled', false);
+    } else {
+      var formData = JSON.stringify({
+        	"fullname":fullname,
+        	"phonenumber":phonenumber,
+        	"acctnum":acctnum,
+        	"password":password,
+        	"mememail":mememail,
+        	"address":address,
+        	"firstcode":rcode,
+        	"occupation":occupation,
+        	"bankname":bankname
+        });
+        console.log(formData);
+        xhr.onreadystatechange = function() {//Call a function when the state changes.
+           if(xhr.readyState === 4) {
+             console.log(this.responseText);
+             var response = JSON.parse(this.responseText);
+             var responseMsg = response["message"];
+             if(response["status"] === true) {
+               swal({
+                   title: "Payment Method",
+                   text: responseMsg,
+                   icon: "info",
+                   buttons: ["Pay at Login", "Pay Now"]
+                 })
+                 .then(function(willPay) {
+                   if (willPay) {
+                     payWithPaystack(mememail);
+
+                   } else {
+                     $('#registerBtn').attr('Submit');
+                     $('#registerBtn').attr('disabled', false);
+                     $('#registerBtn').attr('value', "Submit");
+                     swal("Please Note", "Your account will not be activated until you make payment. Visit https://nairastash.com/pages/login to pay at login", "info");
+                   }
+                 });
+             } else {
+               swal("Error registering", responseMsg, "warning");
+               $('#registerBtn').attr('disabled', false);
+             }
+           }
+        }
+        xhr.open("POST", apidomain+"/register.php");
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.send(formData);
+    }
+  });
+
+
+  //Pay With Paystack
+  function payWithPaystack(mememail){
+    swal("Please wait while payment page loads...", {
+     buttons: false,
+     timer: 3000
+    });
+
+    var handler = PaystackPop.setup({
+        key: 'pk_live_c681b5e7255499bb4e656973403e9d72bcc2dcee',
+        email: mememail,
+        amount: 200000,
+        ref: 'PE_REG_FEE' + Math.floor((Math.random() * 1000000000) + 1),
+        callback: function(response) {
+            /* swal("Please wait while we get response from paystack", {
+             buttons: false,
+             timer: 4000
+           }); */
+            var xhr = new XMLHttpRequest();
+            xhr.withCredentials = false;
+            xhr.addEventListener("readystatechange", function() {
+                if (this.readyState === 4) {
+                    var a = JSON.parse(this.responseText);
+                    if (a.status == "success") {
+                        var xhr = new XMLHttpRequest();
+                        xhr.withCredentials = false;
+                        xhr.addEventListener("readystatechange", function() {
+                            if (this.readyState === 4) {
+                                var a = JSON.parse(this.responseText);
+                                if (a.status == true) {
+                                    swal("Account Activated!", a.message, "success");
+                                } else {
+                                  swal("Account Activation Failed", a.message, "error");
+                                }
+                            }
+                        });
+
+                        xhr.open("GET", apidomain+"/logpayment/?mememail=" + mememail + "&refcode=" + response.reference);
+                        xhr.send(data);
+                    } else {
+                      swal("Payment completed!", "Your account will be activated within 24 hours. We'll notify you when your account is active. Thanks", "success");
+                    }
+                }
+            });
+
+            xhr.open("GET", "https://api.paystack.co/transaction/verify/" + response.reference);
+            xhr.setRequestHeader("authorization",
+                "Bearer sk_live_ca522dfef89254637cb49578d01425d4433bca62");
+            xhr.send();
+        },
+        onClose: function() {
+
+        }
+    });
+    handler.openIframe();
+  }
+
+
+  // check if user is logged in
+  var getUN = getCookie("useremail");
+  if (getUN !== "") {
+      window.location.assign(webdomain+"/pages/profile");
+  }
+
+});
