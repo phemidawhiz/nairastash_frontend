c08883e5e930f211ecd3b04f7cf1cb5307da987d
Ayodele Olufemi Segun <feminiscence@gmail.com>
2018-10-12 12:07:54 +0100

    files for profile management

---
+++ profile/viewprofile.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Personal Card Widget Flat Responsive Widget Template :: w3layouts</title>
+<!-- custom-theme -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Personal Card Widget Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template,
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+		function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- //custom-theme -->
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<link href="../../assets/fa/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<!-- js -->
+<script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+<!-- //js -->
+<link rel="stylesheet" href="css/flexslider.css" type="text/css" media="screen" property="" />
+<link href="//fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i&amp;subset=latin-ext" rel="stylesheet">
+</head>
+<body>
+	<div class="main"><br>
+    <div class="w3_agile_main_grids">
+			<div class="w3_agileits_personal_card_grid">
+				<div class="wthree_personal_card_gridl">
+					<img src="images/1.png" alt=" " />
+				</div>
+				<div class="wthree_personal_card_gridr">
+					<h2 id="fullNamev">Laura Michael</h2>
+					<p id="memEmailv">123 Diamonds ER34 Avenue, London.</p>
+				</div>
+				<div class="clear"> </div>
+				<div class="agileinfo_personal_text">
+					<p >Joined the community on <span id="memRegDatev"></span></p>
+				</div>
+				<div class="agileits_plans">
+					<ul>
+						<li>PHONE: <span id="memPhonev">Curabitur vitae quam</span></li>
+						<li>OCCUPATION: <span id="memWorkv">Curabitur vitae quam</span></li>
+						<li>REG. CODE: <span id="memCodev">Curabitur vitae quam</span></li>
+						<li>ADDRESS: <span id="memAddressv">Curabitur vitae quam</span></li>
+					</ul>
+
+					<br>
+
+				</div>
+			</div>
+		</div>
+  </div>
+  <!-- //flexSlider -->
+	<script src="../../js/jquery-2.1.4.min.js"></script>
+	<script src="../../js/functions.js"></script>
+	<script src="js/profile.js"></script>
+	<script type="text/javascript" src="js/bootstrap.js"></script><!-- bootstrap js file -->
+
+
+</body>
+</html>
